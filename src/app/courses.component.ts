2e54031bf965b8b2822690126c635535c03235d1
Arafat <arafat97alim@gmail.com>
2022-01-29 20:31:50 +0530

    creating a component manually.

---
+++ src/app/courses.component.ts
@@ -0,0 +1,14 @@
+//!  creating component class using PasCal
+import { Component } from '@angular/core'; //decorator import
+
+//!  decorator
+@Component({
+  selector: 'courses',
+  template: '<h2>Courses Department</h2>',
+})
+export class CoursesComponent {}
+
+//css selector
+// <courses></cousres>
+
+// html markup
