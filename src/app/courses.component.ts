841b61c298298811547d912bbc33897c8460b733
Arafat <arafat97alim@gmail.com>
2022-01-31 00:47:03 +0530

    added bootstrap and modify css files.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,15 +7,27 @@ import { CoursesService } from './courses.service';
   selector: 'courses',
   //   template: '<h2>{{"Title : "+ title}}</h2>',
   template: `
+    <!-- adding button -->
+    <input type="button" class="btn btn-primary" value="Save" />
     <h2>{{ getTitle() }}</h2>
+    <h2 [textContent]="title"></h2>
     <ul>
       <li *ngFor="let course of courses">{{ course }}</li>
     </ul>
+    <img src="{{ imageUrl }}" />
+    <img [src]="imageUrl" />
+    <table>
+      <tr>
+        <td [attr.colSpan]="colSpan" ]></td>
+      </tr>
+    </table>
   `,
 })
 export class CoursesComponent {
   title = 'Angular Coursed On demand';
   courses;
+  colSpan = '2';
+  imageUrl = '';
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
