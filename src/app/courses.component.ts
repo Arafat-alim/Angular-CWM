4b9b0d5a67e464659d23bd2c64355669f9049840
Arafat <arafat97alim@gmail.com>
2022-01-31 18:15:22 +0530

    updated back to normal page.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,49 +5,9 @@ import { CoursesService } from './courses.service';
 //!  decorator
 @Component({
   selector: 'courses',
-  //   template: '<h2>{{"Title : "+ title}}</h2>',
-  template: `
-    <!-- adding button -->
-    <input
-      type="button"
-      class="btn btn-primary"
-      [class.active]="isActive"
-      value="Save"
-    />
-    <!-- --------- -->
-    <h2>{{ getTitle() }}</h2>
-    <h2 [textContent]="title"></h2>
-    <ul>
-      <li *ngFor="let course of courses">{{ course }}</li>
-    </ul>
-    <img src="{{ imageUrl }}" />
-    <img [src]="imageUrl" />
-    <table>
-      <tr>
-        <td [attr.colSpan]="colSpan" ]></td>
-      </tr>
-    </table>
-    <button [style.backgroundColor]="isActive ? 'blue' : 'red'" ]>
-      Saving
-    </button>
-  `,
+  template: '<h2>{{"Title : "+ title}}</h2>',
 })
 export class CoursesComponent {
-  isActive = 'true';
-  title = 'Angular Coursed On demand';
-  courses;
-  colSpan = '2';
-  imageUrl = '';
-
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
-  }
-
-  getTitle() {
-    return this.title;
-  }
+  public title = 'Arafat';
+  constructor(service: CoursesService) {}
 }
-
-//css selector
-// <courses></cousres>
-// html markup
