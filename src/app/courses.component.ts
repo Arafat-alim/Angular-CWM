dec0d45e9f23e8c155c184c4d7596acf0eaa9d58
Arafat <arafat97alim@gmail.com>
2022-01-31 23:12:58 +0530

    code is not woking on this platform.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,9 +5,17 @@ import { CoursesService } from './courses.service';
 //!  decorator
 @Component({
   selector: 'courses',
-  template: '<h2>{{"Title : "+ title}}</h2>',
+  template: `
+    <input type="text" (keyup)="onKey($event)" />
+    <h2>{{ 'Title : ' + title }}</h2>
+  `,
 })
 export class CoursesComponent {
+  onKey($event) {
+    if ($event.keyCode === 13) {
+      console.log('Enter Key presses');
+    }
+  }
   public title = 'Arafat';
   constructor(service: CoursesService) {}
 }
